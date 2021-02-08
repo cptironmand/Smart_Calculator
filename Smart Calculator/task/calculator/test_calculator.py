fe50e2df460d22475adca73e8a641a782f34916b
cptironmand <jaymandjunk@gmail.com>
2021-02-07 19:46:01 -0600

    3.0 Completed

---
+++ Smart Calculator/task/calculator/test_calculator.py
@@ -0,0 +1,23 @@
+import unittest
+import calculator
+
+
+class TestCalculator(unittest.TestCase):  # a test case for the calculator.py module
+
+    def test_take_inputs(self):
+        # tests the input modes
+        self.assertEqual(calculator.take_inputs('17 9'),(True, ['17', '9']))
+
+    def test_add(self):
+        # tests for the add() function
+        self.assertEqual(calculator.add(['17', '9']), 26)
+        self.assertEqual(calculator.add(['-2', '5']), 3)
+        #self.assertEqual(calculator.add(['3']), False)
+        self.assertEqual(calculator.add(['6', '4']), 10)
+
+    def test_playground(self):
+        self.assertEqual(calculator.playground("word"), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
